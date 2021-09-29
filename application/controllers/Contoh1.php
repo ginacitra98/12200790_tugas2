72ab56e201f592a5ceb2d5c87ee3bf925c30099c
ginacitra98 <nopitasariginacitra@gmail.com>
2021-09-29 12:53:09 +0100

    tugas2

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1> Perkenalkan</h1>";
+        echo "Nama Saya Gina Citra Nopitasari
+        Saya Tinggal Di Kp. Saripin
+        No. Hp 0852-2143-5175";
+    }
+}
